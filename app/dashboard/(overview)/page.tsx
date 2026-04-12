58b6f0b44764246e455d64b95769d9340197805b
explore0916 <51083328+explore0916@users.noreply.github.com>
2026-04-12 20:30:15 +0800

    第九章-练习-<Suspense>-<LatestInvoicesSkeleton>

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,11 +4,11 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {  fetchLatestInvoices,fetchCardData } from '@/app/lib/data';
 import {Suspense} from 'react';
-import {RevenueChartSkeleton} from '@/app/ui/skeletons';
+import {RevenueChartSkeleton, LatestInvoicesSkeleton} from '@/app/ui/skeletons';
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
   
   return (
@@ -31,7 +31,10 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
         
       </div>
     </main>
